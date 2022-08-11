a696d0195a6d4298bcc0804b8b2ac0d66aadd08b
Joost Hoozemans <joosthooz@msn.com>
2022-08-11 12:14:01 +0200

    13 solidify cache hierarchy level names (#39)
    * Refactored cached_dataset path building into a function
    
    * Added scalefactor_ and partitioning_ prefixes in cache hierarchy naming
    
    * Fix in partitioning_ prefix
    
    * Renaming un-partitioned datasets to direct filenames, instead of a part-0 file
    
    * Fixed unit test

--- datalogistik/datalogistik.py
+++ datalogistik/datalogistik.py
@@ -47,8 +47,7 @@ def main():
     # check if an existing dataset is still valid.
 
     log.debug(f"Checking local cache at {local_cache_location}")
-    cached_dataset_path = pathlib.Path(
-        local_cache_location,
+    cached_dataset_path = util.create_cached_dataset_path(
         argument_info.dataset,
         argument_info.scale_factor,
         argument_info.format,
@@ -71,7 +70,7 @@ def main():
             other_format_path = pathlib.Path(
                 local_cache_location,
                 argument_info.dataset,
-                argument_info.scale_factor,
+                f"scalefactor_{argument_info.scale_factor}",
                 cached_file_format,
             )
             if other_format_path.exists():
@@ -85,6 +84,7 @@ def main():
                 ]
                 for cached_nrows in subfolders:
                     other_nrows_path = pathlib.Path(other_format_path, cached_nrows)
+                    cached_nrows = cached_nrows.split("_")[-1]
                     cached_dataset_metadata_file = pathlib.Path(
                         other_nrows_path, config.metadata_filename
                     )
@@ -97,7 +97,6 @@ def main():
                             f"Metadata file located at '{cached_dataset_metadata_file}'"
                         )
                         cached_dataset_path = util.convert_dataset(
-                            local_cache_location,
                             dataset_info,
                             argument_info.compression,
                             cached_file_format,
@@ -122,20 +121,15 @@ def main():
     log.info("Dataset not found in local cache.")
 
     if argument_info.dataset in tpc_info.tpc_datasets:
-        cached_dataset_path = util.generate_dataset(
-            dataset_info, argument_info, local_cache_location
-        )
+        cached_dataset_path = util.generate_dataset(dataset_info, argument_info)
     else:
-        cached_dataset_path = util.download_dataset(
-            dataset_info, argument_info, local_cache_location
-        )
+        cached_dataset_path = util.download_dataset(dataset_info, argument_info)
 
     # Convert to the requested format if necessary
     if (dataset_info["format"] != argument_info.format) or (
         dataset_info["partitioning-nrows"] != argument_info.partition_max_rows
     ):
         cached_dataset_path = util.convert_dataset(
-            local_cache_location,
             dataset_info,
             argument_info.compression,
             dataset_info["format"],
