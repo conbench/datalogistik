bc9d920035f2afdfa274fd2d13690c0abc184bb4
Joost Hoozemans <joosthooz@msn.com>
2022-09-20 15:22:43 +0200

    Removed some code that should not be reachable

--- datalogistik/dataset_search.py
+++ datalogistik/dataset_search.py
@@ -15,7 +15,6 @@
 import copy
 
 from . import repo, tpc_info, util
-from .log import log
 
 
 def find_exact_dataset(dataset):
@@ -36,6 +35,7 @@ def find_close_dataset(dataset):
 
     # If no variants are available, ensure that one is
     if variants == []:
+        # N. B. this will raise an error for unrecognized datasets
         dataset_to_fetch = repo.search_repo(dataset.name, repo.get_repo())
         if dataset_to_fetch:
             # we found a dataset, so we can use it
@@ -50,12 +50,6 @@ def find_close_dataset(dataset):
         if variants == []:
             # this is generatable + must be generated
             variants = [util.generate_dataset(dataset)]
-
-    # We still can't find any variants!
-    if variants == []:
-        msg = f"Unknown dataset {dataset.name}"
-        log.error(msg)
-        raise RuntimeError(msg)
 
     # order parquet first (sicne they should be fast(er) to convert from)
     # when we support .arrow, those likely should be first, then parquet, et c.
