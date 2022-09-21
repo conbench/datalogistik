b066e3288fde13910ad0713aef3ea130080e16a2
Jonathan Keane <jkeane@gmail.com>
2022-09-21 14:45:27 -0500

    oops, default

--- datalogistik/datalogistik.py
+++ datalogistik/datalogistik.py
@@ -30,7 +30,7 @@ def finish():
     sys.exit(0)
 
 
-def main(dataset):
+def main(dataset=cli.parse_args_and_get_dataset_info()):
     if config.get_max_cpu_count() != 0:
         pyarrow.set_cpu_count(config.get_max_cpu_count())
         pyarrow.set_io_thread_count(config.get_max_cpu_count())
